ea3678bee970e3f05309cb3e0717e78999e22f57
shadab1928raza <shadab.razamd@gmail.com>
2022-09-07 09:34:18 +0530

    All the routes of the project has been completed

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const route = require('./routes/route.js');
+const { default: mongoose } = require('mongoose');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb+srv://shadab_1928:shadab_1928@cluster0.zb6nc0e.mongodb.net/Mini_Blog-DB?retryWrites=true&w=majority", {
+  useNewUrlParser: true
+})
+  .then(() => console.log('MongoDb is connected'))
+  .catch((err) => console.log(err));
+
+app.use('/', route);
+
+app.all('/**', (req, res) => {
+  res.status(404).send({ status: false, message: 'Page Not Found!' });
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Express app running on port ' + (process.env.PORT || 3000));
+});
